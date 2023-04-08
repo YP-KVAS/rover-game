64c81ce907b66b48dca743851238e0b98cba2013
Katerina Driga <katherinedriga@yandex.ru>
2023-04-08 23:00:55 +0300

    fix: IUserPassword interface import

--- packages/client/src/utils/rest-api/user-api.ts
+++ packages/client/src/utils/rest-api/user-api.ts
@@ -1,6 +1,6 @@
 import { FetchMethods, request } from './base-request'
 import { USER_API_URL, UserApiPaths } from '../const-variables/api'
-import { User, UserPassword, UserSettings } from '../types/user'
+import { User, IUserPassword, UserSettings } from '../types/user'
 
 export async function changeProfileSettings(data: UserSettings): Promise<User> {
   return await request(`${USER_API_URL}${UserApiPaths.CHANGE_SETTINGS}`, {
@@ -18,7 +18,7 @@ export async function changeAvatar(avatar: File): Promise<User> {
   })
 }
 
-export async function changePassword(data: UserPassword): Promise<void> {
+export async function changePassword(data: IUserPassword): Promise<void> {
   return await request(`${USER_API_URL}${UserApiPaths.CHANGE_PASSWORD}`, {
     method: FetchMethods.PUT,
     data,
