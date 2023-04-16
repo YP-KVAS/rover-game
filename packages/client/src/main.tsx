612cbbb84be4403c9bf2a5662a3dca503adbee42
sergh.al <sergh.al@yandex.ru>
2023-04-16 23:29:17 +0300

    Add service worker

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -5,6 +5,26 @@ import './index.scss'
 import { Provider } from 'react-redux'
 import { store } from './store/store'
 
+const startServiceWorker = () => {
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker
+        .register('/sw.js')
+        .then(registration => {
+          console.log(
+            'ServiceWorker успешно зарегистрирован: ',
+            registration.scope
+          )
+        })
+        .catch((error: string) => {
+          console.log('Регистрация ServiceWorker завершилась ошибкой: ', error)
+        })
+    })
+  }
+}
+
+startServiceWorker()
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
