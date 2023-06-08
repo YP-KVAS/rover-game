9f1578ba92ea4400c19c3a9d34dc45b51844d129
Katerina Driga <katherinedriga@yandex.ru>
2023-06-08 19:33:43 +0300

    chore: env var import.meta.env?.VITE_BASE_URL

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import dotenv from 'dotenv'
 import * as path from 'path'
-import EnvironmentPlugin from 'vite-plugin-environment'
 dotenv.config()
 
 // https://vitejs.dev/config/
@@ -13,7 +12,7 @@ export default defineConfig({
   define: {
     __SERVER_PORT__: process.env.SERVER_PORT || 5000,
   },
-  plugins: [react(), EnvironmentPlugin('all')],
+  plugins: [react()],
   resolve: {
     // empty file needed to avoid vite errors caused by sanitize-html
     alias: {
